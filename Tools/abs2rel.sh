9d0850f1103b42791ac680d65a710b1162068b30
Michael Böhme <boehme.mic@gmail.com>
2018-09-28 18:53:52 +0200

    comment adjusted

--- Tools/abs2rel.sh
+++ Tools/abs2rel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# alpha, beta, energy, rel. energy (a.u.), rel. energy (1/cm)
+# alpha, beta, energy, abs. energy (a.u.), rel. energy (a.u.), rel. energy (1/cm)
 
 if [ -f "$1" ]; then
 	MINVAL=`awk 'NR==1 {min=$3}; NR>1 && $3<min && $3>0 {min=$3}; END{print min}' "$1"`
